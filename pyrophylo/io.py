5265a00b09eb87174178d27cd2cf52828e914836
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2020-09-01 16:10:59 -0600

    Add initial Phylogeny data structure

--- pyrophylo/io.py
+++ pyrophylo/io.py
@@ -1,13 +1,18 @@
+import math
+from contextlib import ExitStack
+
+import torch.multiprocessing as mp
 from Bio.Phylo.NewickIO import Parser
+
+from .phylo import Phylogeny
 
 
 def _handle_translate(lines, context):
     map_lines = [line.rstrip(",").split() for line in lines[1:-1]]
     context["translate"] = {key: value for key, value in map_lines}
-    return ()
 
 
-def _handle_tree(lines, context):
+def _handle_tree_newick(lines, context):
     assert len(lines) == 1
     tree, name, equal, newick = lines[0].split()
     assert tree == "tree"
@@ -21,32 +26,79 @@ def _handle_tree(lines, context):
         for leaf in tree.get_terminals():
             leaf.comment = translate[leaf.name]
 
-    yield tree
+    return tree
 
 
-_HANDLERS = {
-    "translate": _handle_translate,
-    "tree": _handle_tree,
-}
+def _handle_tree_torch(lines, context):
+    assert len(lines) == 1
+    tree, name, equal, newick = lines[0].split()
+    assert tree == "tree"
+    assert equal == "="
+    tree = next(Parser.from_string(newick).parse())
+    return Phylogeny.from_bio_phylo(tree)
 
 
-def parse_nexus_trees(filename):
+def _handle_async(pool, fn):
+    def handle(lines, context):
+        return pool.apply_async(fn, (lines, context))
+    return handle
+
+
+def iter_nexus_trees(filename, *, format="newick", processes=0):
     """
     Parse and iterate over newick trees stored in a nexus file.
     This streams the file and thus can handle larger files than
     ``Bio.Phylo.read(..., format="nexus")``.
+
+    Returns an iterator of ``Bio.Phylo`` tree objects.
     """
-    with open(filename) as f:
-        lines = iter(f)
+    if format == "newick":
+        context = {"translate": {}}
+        handlers = {"translate": _handle_translate,
+                    "tree": _handle_tree_newick}
+    elif format == "torch":
+        context = None
+        handlers = {"tree": _handle_tree_torch}
+    else:
+        raise ValueError(f"unknown format: {format}")
+
+    with ExitStack() as stack:
+        # Optionally generate async results.
+        if processes > 0:
+            mp.set_start_method("spawn")
+            pool = stack.enter_context(mp.Pool(processes))
+            handlers["tree"] = _handle_async(pool, handlers.pop("tree"))
+
+        # Skip until trees.
+        lines = iter(stack.enter_context(open(filename)))
         for line in lines:
             if line.startswith("Begin trees;"):
                 break
-        context = {"translate": {}}
+
         part = []
         for line in lines:
             line = line.strip()
             part.append(line)
-            if line.endswith(";"):
-                type_ = part[0].split()[0].lower()
-                yield from _HANDLERS[type_](part, context)
-                part = []
+            if not line.endswith(";"):
+                continue
+            type_ = part[0].split()[0].lower()
+            handle = handlers.get(type_)
+            if handle is not None:
+                tree = handle(part, context)
+                if tree is not None:
+                    yield tree
+            part = []
+
+
+def stack_nexus_trees(filename, *, max_num_trees=math.inf, processes=0, timeout=None):
+    """
+    Loads a batch of trees from a nexus file.
+    """
+    phylogenies = []
+    for tree in iter_nexus_trees(filename, format="torch", processes=processes):
+        phylogenies.append(tree)
+        if len(phylogenies) >= max_num_trees:
+            break
+    if processes > 0:
+        phylogenies = [async_result.get(timeout) for async_result in phylogenies]
+    return Phylogeny.stack(phylogenies)
--- test/test_io.py
+++ test/test_io.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from Bio import Phylo
 
-from pyrophylo.io import parse_nexus_trees
+from pyrophylo.io import iter_nexus_trees, stack_nexus_trees
 
 ROOT = os.path.dirname(os.path.dirname(__file__))
 FILENAME = os.path.join(ROOT, "data", "GTR4G_posterior.trees")
@@ -16,8 +16,31 @@ def test_bio_phylo_parse(args):
         print(tree.count_terminals())
 
 
-def test_parse_nexus_trees():
-    for i, tree in enumerate(parse_nexus_trees(FILENAME)):
+def test_iter_nexus_trees():
+    for i, tree in enumerate(iter_nexus_trees(FILENAME)):
         assert tree.count_terminals() == 772
         if i == 5:
             break
+
+
+def test_stack_nexus_trees():
+    phylo = stack_nexus_trees(FILENAME, max_num_trees=5)
+    assert phylo.batch_shape == (5,)
+
+
+@pytest.mark.xfail(reason="unknown")
+def test_iter_nexus_trees_async():
+    trees = []
+    for i, tree in enumerate(iter_nexus_trees(FILENAME, processes=2)):
+        trees.append(tree)
+        if i == 5:
+            break
+    for async_result in trees:
+        tree = async_result.get(timeout=0.5)
+        assert tree.count_terminals() == 772
+
+
+@pytest.mark.xfail(reason="unknown")
+def test_stack_nexus_trees_async():
+    phylo = stack_nexus_trees(FILENAME, max_num_trees=5, processes=2, timeout=None)
+    assert phylo.batch_shape == (5,)
