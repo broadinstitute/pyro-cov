ee7e1411cfab222d51318591670296bca63ce94a
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-02-04 12:23:17 -0500

    Get us_lineage_model notebook working

--- pyrophylo/pangolin.py
+++ pyrophylo/pangolin.py
@@ -1,6 +1,9 @@
+from collections import Counter
+
 import torch
 
-# See https://cov-lineages.org/lineages.html
+# See https://cov-lineages.org/lineages.html or
+# https://github.com/cov-lineages/pangoLEARN/blob/master/pangoLEARN/supporting_information/lineage_notes.txt
 PANGOLIN_ALIASES = {
     "C": "B.1.1.1",
     "D": "B.1.1.25",
@@ -10,28 +13,63 @@ PANGOLIN_ALIASES = {
     "H": "B.1.1.67",
     "I": "B.1.1.217",
     "J": "B.1.1.250",
-    "K": "B.1.1.277",  # FIXME
+    "K.1": "B.1.1.277",
     "L": "B.1.1.10",
     "M": "B.1.1.294",
     "N": "B.1.1.33",
     "P": "B.1.1.28",
 }
 
+DECOMPRESS = {}
+COMPRESS = {}
 
-def canonize(name):
-    prefix = name[0]
-    if prefix in "AB":
+
+def decompress(name):
+    """
+    Decompress an alias like C.10 to a full lineage like B.1.1.1.10.
+    """
+    try:
+        return DECOMPRESS[name]
+    except KeyError:
+        pass
+    if name[0] in "AB":
+        DECOMPRESS[name] = COMPRESS[name] = name
         return name
-    return PANGOLIN_ALIASES[prefix] + name[1:]
+    for key, value in PANGOLIN_ALIASES.items():
+        if name.startswith(key):
+            result = value + name[len(key):]
+            DECOMPRESS[name] = result
+            COMPRESS[result] = name
+            return result
+    raise ValueError(f"Unknown alias: {name}")
+
+
+def compress(name):
+    """
+    Compress a full lineage like B.1.1.1.10 to an alias like C.10.
+    """
+    return COMPRESS[name]
+
+
+def _get_parent(longname):
+    return longname.rsplit(".", 1)[0]
 
 
 def find_edges(names):
-    short_to_long = {name: canonize(name) for name in names}
+    """
+    Given a set of short lineages, return a list of pairs of parent-child
+    relationships among lineages.
+    """
+    short_to_long = {name: decompress(name) for name in names}
     long_to_short = {v: k for k, v in short_to_long.items()}
     assert len(short_to_long) == len(long_to_short)
     edges = [("A", "B")]
     for x, longx in short_to_long.items():
-        longy = longx.rsplit(".", 1)[0]
+        longy = _get_parent(longx)
+        while longy not in names:
+            longy = _get_parent(longy)
+        if longy == longx:
+            continue
         if longy != longx:
             y = long_to_short[longy]
             assert x != y
@@ -41,7 +79,43 @@ def find_edges(names):
     return edges
 
 
+def merge_lineages(counts, min_count):
+    """
+    Given a dict of lineage counts and a min_count, returns a mapping from all
+    lineages to merged lineages.
+    """
+    assert isinstance(counts, dict)
+    assert isinstance(min_count, int)
+    assert min_count > 0
+
+    # Merge rare children into their parents.
+    counts = Counter({decompress(k): v for k, v in counts.items()})
+    mapping = {}
+    for child in sorted(counts, key=lambda k: (-len(k), k)):
+        if counts[child] < min_count:
+            parent = _get_parent(child)
+            if parent == child:
+                continue  # at a root
+            counts[parent] += counts.pop(child)
+            mapping[child] = parent
+
+    # Transitively close.
+    for old, new in list(mapping.items()):
+        while new in mapping:
+            new = mapping[new]
+        mapping[old] = new
+
+    # Recompress.
+    mapping = {compress(k): compress(v) for k, v in mapping.items()}
+    return mapping
+
+
 def classify(lineage):
+    """
+    Given a list of compressed lineages, return a torch long tensor of class
+    ids and a list of pairs of integers representing parent-child lineage
+    relationships between classes.
+    """
     # Construct a class tensor.
     names = sorted(set(lineage))
     position = {name: i for i, name in enumerate(names)}
@@ -50,7 +124,7 @@ def classify(lineage):
         classes[i] = position[name]
 
     # Construct a tree.
-    short_to_long = {name: canonize(name) for name in names}
+    short_to_long = {name: decompress(name) for name in names}
     long_to_short = {v: k for k, v in short_to_long.items()}
     assert len(short_to_long) == len(long_to_short)
     edges = [(position["A"], position["B"])]
