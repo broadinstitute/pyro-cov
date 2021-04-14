6c9b594b64de71ce36de9a8281d46656d419ae8d
jy17 <lemieux@broadinstitute.org>
2021-04-14 15:38:39 -0400

    update fig1 plots

--- pyrocov/plot_additional_figs.R
+++ pyrocov/plot_additional_figs.R
@@ -4,13 +4,13 @@
 
 library(tidyverse)
 library(cowplot)
-top_strains <- read_tsv("~/Dropbox/COVID/pyro-cov/paper/top_strains.tsv")[,1:3]
-names(top_strains) <- c("strain", "log_growth_rate", "num_samples")
-top_strains <- top_strains %>% mutate(log_samples = log10(num_samples)) 
-p1 <- ggplot(top_strains, aes(x = log_growth_rate, y = log_samples, label = strain)) + geom_text(size = 3) + theme_bw() + geom_smooth(method = "lm")
-p2 <- ggplot(top_strains, aes(x = log_growth_rate)) + geom_histogram(binwidth = 1) + theme_bw()
+top_strains <- read_tsv("~/Dropbox/COVID/pyro-cov/paper/strains.tsv")[,1:3]
+names(top_strains) <- c("strain", "R_eff", "Cases_per_day")
+top_strains <- top_strains %>% mutate(log_samples = log10(Cases_per_day)) 
+p1 <- ggplot(top_strains, aes(x = R_eff, y = log_samples, label = strain)) + geom_text(size = 3) + theme_bw() + geom_smooth(method = "lm")
+p2 <- ggplot(top_strains, aes(x = R_eff)) + geom_histogram(binwidth = 0.05) + theme_bw()
 
-m1 <- lm(data = top_strains, log_samples ~ log_growth_rate)
+m1 <- lm(data = top_strains, Cases_per_day ~ R_eff)
 summary(m1)
 
 plot_grid(p1, p2, nrow = 1, labels = c("A", "B"))
