83d183ed8ed7403628e57d23fc925600c7bd0c4f
Nick Barkas <nbarkas@broadinstitute.org>
2021-07-14 17:59:43 +0000

    add backtesting bash script call

--- run_backtesting.sh
+++ run_backtesting.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python mutrans.py --backtesting-max-day 150,200,250,300,350,400,450,500,550
+backtesting_days=$(seq -s, 150 7 550)
+echo $backtesting_days
+#python mutrans.py --backtesting-max-day 150,200,250,300,350,400,450,500,550
+python mutrans.py --backtesting-max-day $backtesting_days
