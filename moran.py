6e3939c664cffed76714c2b4d5d0fd81bc0bed46
Martin Jankowiak <jankowiak@gmail.com>
2021-07-17 17:19:24 -0400

    update/clean-up moran.py

--- moran.py
+++ moran.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import defaultdict
 
 import numpy as np
@@ -11,67 +12,65 @@ from pyrocov.sarscov2 import aa_mutation_to_position
 def moran(values, distances, lengthscale):
     assert values.size(-1) == distances.size(-1)
     weights = (distances.unsqueeze(-1) - distances.unsqueeze(-2)) / lengthscale
-    weights = weights.pow(2.0)
-    weights = torch.exp(-weights)
+    weights = torch.exp(-weights.pow(2.0))
+    weights *= (1.0 - torch.eye(weights.size(-1)))
     weights /= weights.sum(-1, keepdim=True)
 
-    return torch.einsum("...ij,...i,...j->...", weights, values, values) / values.pow(
-        2.0
-    ).sum(-1)
+    output = torch.einsum("...ij,...i,...j->...", weights, values, values)
+    return output / values.pow(2.0).sum(-1)
 
 
 # compute moran statistic and do permutation test with given number of permutations
-def permutation_test(values, distances, lengthscale, num_permutations=9999):
+def permutation_test(values, distances, lengthscale, num_perm=9999):
     values = values - values.mean()
     moran_given = moran(values, distances, lengthscale).item()
-    idx = torch.stack(
-        [torch.randperm(distances.size(-1)) for _ in range(num_permutations)]
-    )
+    idx = [torch.randperm(distances.size(-1)) for _ in range(num_perm)]
+    idx = torch.stack(idx)
     moran_perm = moran(values[idx], distances, lengthscale)
     p_value = (moran_perm >= moran_given).sum().item() + 1
-    p_value /= float(num_permutations + 1)
+    p_value /= float(num_perm + 1)
     return moran_given, p_value
 
 
-# read in inferred mutations
-df = pd.read_csv("paper/mutations.tsv", sep="\t", index_col=0)[["mutation", "Δ log R"]]
-mutations = df.values[:, 0]
-assert mutations.shape == (2337,)
-coefficients = df.values[:, 1]
-gene_map = defaultdict(list)
-distance_map = defaultdict(list)
+def main(args):
+    # read in inferred mutations
+    df = pd.read_csv("paper/mutations.tsv", sep="\t", index_col=0)[["mutation", "Δ log R"]]
+    mutations = df.values[:, 0]
+    assert mutations.shape == (2337,)
+    coefficients = df.values[:, 1] if not args.magnitude else np.abs(df.values[:, 1])
+    gene_map = defaultdict(list)
+    distance_map = defaultdict(list)
 
-# collect indices corresponding to each gene
-for i, m in enumerate(mutations):
-    gene = m.split(":")[0]
-    gene_map[gene].append(i)
-    distance_map[gene].append(aa_mutation_to_position(m))
+    # collect indices and nucleotide positions corresponding to each mutation
+    for i, m in enumerate(mutations):
+        gene = m.split(":")[0]
+        gene_map[gene].append(i)
+        distance_map[gene].append(aa_mutation_to_position(m))
 
-# map over each gene
-for gene, idx in gene_map.items():
-    values = torch.from_numpy(np.array(coefficients[idx], dtype=np.float32))
-    distances = distance_map[gene]
-    distances = torch.from_numpy(np.array(distances) - min(distances))
-    gene_size = distances.max().item()
-    lengthscale = min(gene_size / 20, 50.0)
-    _, p_value = permutation_test(
-        values, distances, lengthscale, num_permutations=99999
-    )
-    print(
-        "Gene: {} \t p-value: {:.4f}  Lengthscale: {:.1f}".format(
-            gene, p_value, lengthscale
-        )
-    )
+    # map over each gene
+    for gene, idx in gene_map.items():
+        values = torch.from_numpy(np.array(coefficients[idx], dtype=np.float32))
+        distances = distance_map[gene]
+        distances = torch.from_numpy(np.array(distances) - min(distances))
+        gene_size = distances.max().item()
+        lengthscale = min(gene_size / 20, 50.0)
+        _, p_value = permutation_test(values, distances, lengthscale, num_perm=99999)
+        s = "Gene: {} \t Size: {} \t p-value: {:.6f}  Lengthscale: {:.1f}"
+        print(s.format(gene, gene_size, p_value, lengthscale))
 
-global_lengthscale = 500.0
-distances_ = [aa_mutation_to_position(m) for m in mutations]
-distances = torch.from_numpy(np.array(distances_, dtype=np.float32) - min(distances_))
-values = torch.tensor(np.array(coefficients, dtype=np.float32)).float()
-_, p_value = permutation_test(
-    values, distances, global_lengthscale, num_permutations=99999
-)
-print(
-    "Entire Genome: \t p-value: {:.4f}  Lengthscale: {:.1f}".format(
-        p_value, global_lengthscale
-    )
-)
+    # compute moran statistic for entire genome for mulitple lengthscales
+    for global_lengthscale in [100.0, 500.0]:
+        distances_ = [aa_mutation_to_position(m) for m in mutations]
+        distances = torch.from_numpy(np.array(distances_, dtype=np.float32) - min(distances_))
+        values = torch.tensor(np.array(coefficients, dtype=np.float32)).float()
+        _, p_value = permutation_test(values, distances, global_lengthscale, num_perm=99999)
+        genome_size = distances.max().item()
+        s = "Entire Genome (Size = {}): \t p-value: {:.6f}  Lengthscale: {:.1f}"
+        print(s.format(genome_size, p_value, global_lengthscale))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compute moran statistics")
+    parser.add_argument("--magnitude", action="store_true")
+    args = parser.parse_args()
+    main(args)
