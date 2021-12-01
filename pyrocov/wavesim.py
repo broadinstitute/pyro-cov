dd0ffb5af84dc052222b7931a55957ed0c37d995
martin jankowiak <jankowiak@gmail.com>
2021-12-01 00:57:13 +0000

    basic sim

--- pyrocov/wavesim.py
+++ pyrocov/wavesim.py
@@ -1,4 +1,6 @@
 import argparse
+from scipy.stats import spearmanr, pearsonr
+import numpy as np
 
 import torch
 from torch.distributions import Bernoulli, Categorical, Multinomial, Normal
@@ -9,8 +11,8 @@ from pyro import distributions as dist
 from pyro.infer.reparam import LocScaleReparam
 
 
-def generate_data(args, sigma1=0.1, sigma2=0.01, sigma3=0.5, sigma4=0.01, tau=10.0):
-    torch.manual_seed(args.seed)
+def generate_data(args, seed=0, sigma1=1.0, sigma2=0.1, sigma3=0.25, sigma4=0.1, tau=10.0):
+    torch.manual_seed(seed)
 
     assert args.num_lineages % args.num_waves == 0
 
@@ -24,31 +26,30 @@ def generate_data(args, sigma1=0.1, sigma2=0.01, sigma3=0.5, sigma4=0.01, tau=10
 
     beta_ps = X @ beta_f + sigma4 * torch.randn(args.num_regions, args.num_lineages)
 
-    #time_shift = Categorical(logits=torch.zeros(args.wave_duration // 2)).sample(sample_shape=(args.num_regions,
-    #                                                                                      args.num_lineages))
-    wave_shift = args.wave_duration * torch.tile(torch.arange(args.num_waves), (args.num_lineages // args.num_waves,))
-    time_shift = 0.0 #wave_shift.float() + time_shift.float() - 0.25 * args.wave_duration
+    time_shift = args.wave_duration * torch.tile(torch.arange(args.num_waves), (args.num_lineages // args.num_waves,))
 
     time = torch.arange(args.num_waves * args.wave_duration)
-    growth_rate = alpha_ps + (time[:, None, None] + time_shift) * beta_ps / tau
-    #multinomial_probs = torch.softmax(growth_rate, dim=-1)
-    #assert multinomial_probs.shape == time.shape + alpha_ps.shape
+    growth_rate = alpha_ps + (time[:, None, None] - time_shift) * beta_ps / tau
+    assert growth_rate.shape == time.shape + alpha_ps.shape
 
-    waveform = Normal(0.85 * args.wave_duration,
-                      0.3 * args.wave_duration).log_prob(torch.arange(args.wave_duration).float()).exp()
+    waveform = Normal(0.5 * args.wave_duration,
+                      0.2 * args.wave_duration).log_prob(torch.arange(args.wave_duration).float()).exp()
     waveform = (args.wave_peak * waveform / waveform.max()).round()
     waveform = torch.tile(waveform, (args.num_waves,))
-    print(waveform)
+    # print(waveform)
 
     counts = [Multinomial(total_count=int(waveform[t].item()), logits=growth_rate[t]).sample() for t in time]
     counts = torch.stack(counts)
 
     assert (args.num_regions * waveform - counts.sum(-1).sum(-1)).abs().max().item() == 0
 
-    print("lineage counts", counts.sum(0).sum(0))
-    print("true beta", beta_f[:args.num_causal_mutations].data.numpy())
+    # print("lineage counts", counts.sum(0).sum(0))
+    print("total count: ", int(counts.sum().item()))
 
-    dataset = {'counts': counts, 'features': X, 'tau': tau}
+    if args.device == 'gpu':
+        counts, X = counts.cuda(), X.cuda()
+
+    dataset = {'counts': counts, 'features': X, 'tau': tau, 'true_coef': beta_f}
     return dataset
 
 
@@ -61,13 +62,6 @@ def model(dataset):
     time_plate = pyro.plate("time", T, dim=-3)
     time = torch.arange(T)
 
-    reparam = {}
-    time_shift = 0.0 # pyro.param("time_shift", lambda: torch.zeros(P, C))  # [P, C]
-    reparam["coef"] = LocScaleReparam()
-    reparam["init_loc"] = LocScaleReparam()
-    reparam["rate"] = LocScaleReparam()
-    reparam["init"] = LocScaleReparam()
-    reparam["rate_loc"] = LocScaleReparam()
     reparam = {}
 
     with poutine.reparam(config=reparam):
@@ -91,7 +85,7 @@ def model(dataset):
             init = pyro.sample("init", dist.Normal(init_loc, init_scale))  # [P, C]
             assert rate.shape == init.shape == (P, C)
 
-        logits = init + rate * (time_shift + time[:, None, None]) / dataset['tau']  # [T, P, C]
+        logits = init + rate * time[:, None, None] / dataset['tau']  # [T, P, C]
         assert logits.shape == (T, P, C)
         with time_plate, place_plate:
             pyro.sample(
@@ -111,33 +105,47 @@ def fit_svi(args, dataset):
         if step % args.report_frequency == 0 or step == args.num_svi_steps - 1:
             print("[step %04d]  loss: %.4f" % (step, loss))
 
-    if 'coef_decentered' in guide.median():
-        print(guide.median()['coef_decentered'].data.numpy())
-    if 'coef' in guide.median():
-        print(guide.median()['coef'].data.numpy())
+    inferred_coef = guide.median()['coef']
+    print("inferred_coef: ", inferred_coef.data.cpu().numpy())
+    print("true_coeff: ", dataset['true_coef'].data.cpu().numpy())
+
+    pearson = pearsonr(inferred_coef.data.cpu().numpy(), dataset['true_coef'].data.cpu().numpy())[0]
+    print("pearson: ", pearson)
+
+    return pearson
 
 
 def main(args):
-    dataset = generate_data(args)
+    print(args)
 
-    model(dataset)
+    pearsons = []
+    num_simulations = 10
+    for simulation in range(num_simulations):
+        dataset = generate_data(args, seed=args.seed + simulation)
+        pearsons.append( fit_svi(args, dataset) )
 
-    fit_svi(args, dataset)
+    print("pearsons", pearsons)
+    print("[# waves: {}]  {:.4f} +- {:.4f}".format(args.num_waves, np.mean(pearsons), np.std(pearsons)))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simulate multiple pandemic waves")
-    parser.add_argument("--num-svi-steps", default=5000, type=int)
+    parser.add_argument("--num-svi-steps", default=3000, type=int)
     parser.add_argument("--report-frequency", default=200, type=int)
     parser.add_argument("--lr", default=0.01, type=float)
     parser.add_argument("--lrd", default=0.1, type=float)
-    parser.add_argument("--num-mutations", default=3, type=int)
-    parser.add_argument("--num-causal-mutations", default=3, type=int)
-    parser.add_argument("--num-lineages", default=50, type=int)
-    parser.add_argument("--num-regions", default=20, type=int)
+    parser.add_argument("--num-mutations", default=20, type=int)
+    parser.add_argument("--num-causal-mutations", default=5, type=int)
+    parser.add_argument("--num-lineages", default=32, type=int)
+    parser.add_argument("--num-regions", default=32, type=int)
     parser.add_argument("--num-waves", default=1, type=int)
     parser.add_argument("--wave-peak", default=1000, type=int)
     parser.add_argument("--seed", default=0, type=int)
     parser.add_argument("--wave-duration", default=100, type=int)
+    parser.add_argument("--device", default='gpu', type=str, choices=['cpu', 'gpu'])
     args = parser.parse_args()
+
+    if args.device == 'gpu':
+        torch.set_default_tensor_type(torch.cuda.FloatTensor)
+
     main(args)
