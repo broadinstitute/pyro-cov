b8e2d09ad40828cb8b7f5a06722ec3236fa1530a
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2022-01-25 02:28:00 +0000

    Fix pull_nextstrain.sh

--- scripts/pull_nextstrain.sh
+++ scripts/pull_nextstrain.sh
@@ -4,4 +4,4 @@ mkdir -p results/nextstrain
 
 curl https://data.nextstrain.org/files/ncov/open/metadata.tsv.gz -o results/nextstrain/metadata.tsv.gz
 
-gunzip -k results/nextstrain/metadata.tsv.gz
+gunzip -kvf results/nextstrain/metadata.tsv.gz
