e09f3f2da61760d4aab889fe3bc8d08a89f9af62
bkotzen <bkotzen@mgh.harvard.edu>
2022-08-30 20:01:11 +0000

    script to run analysis of first four weeks of a variant

--- series_analyze.sh
+++ series_analyze.sh
@@ -16,12 +16,15 @@ echo ""
 echo "--------------------------------------------------------"
 
 declare -a series=(\
-# "2022-02-10 BA.2.12.1_wk0"\
-"2022-02-17 https://hgwdev.gi.ucsc.edu/~angie/9620db8/gisaidAndPublic.2022-02-17.masked.pb.gz BA.2.12.2_wk1 "\
-"2022-02-24 https://hgwdev.gi.ucsc.edu/~angie/6bca5eb/gisaidAndPublic.2022-02-23.masked.pb.gz BA.2.12.1_wk2 "\
-"2022-03-10 https://hgwdev.gi.ucsc.edu/~angie/14b4e4e/gisaidAndPublic.2022-03-10.masked.pb.gz BA.2.12.1_wk4 "\
+# "2022-02-10 BA.2.12.1_wk0" \
+"2022-02-17 https://hgwdev.gi.ucsc.edu/~angie/9620db8/gisaidAndPublic.2022-02-17.masked.pb.gz BA.2.12.2_wk1 " \
+"2022-02-24 https://hgwdev.gi.ucsc.edu/~angie/6bca5eb/gisaidAndPublic.2022-02-23.masked.pb.gz BA.2.12.1_wk2 " \
+"2022-03-10 https://hgwdev.gi.ucsc.edu/~angie/14b4e4e/gisaidAndPublic.2022-03-10.masked.pb.gz BA.2.12.1_wk4 " \
 )
 
-for args in ${series[@]}; do
-  echo $args
+for args in "${series[@]}"; do
+  echo Running on $args
+  source analyze_no_gdown.sh $args
+  echo "----------------------------------------------------------"
 done
+
