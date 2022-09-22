5fbe27bda62056987155a428c68834bee58570fa
bkotzen <bkotzen@mgh.harvard.edu>
2022-09-22 17:36:52 +0000

    updates to analyze scripts

--- analyze_epoch.sh
+++ analyze_epoch.sh
@@ -15,7 +15,7 @@ voc=$1
 d=$2
 
 # Create and link directories
-echo "Creating and linking directories"
+echo ANALYZE_EPOCH.SH "Creating and linking directories"
 # cd pyro-cov
 # d=$(date +%Y-%m-%d)
 # mkdir results.”$d”
@@ -23,7 +23,7 @@ rm results
 ln -sf epochs/$voc/results.$d results
 
 # Download Angie's tree
-echo "Downloading UShER tree"
+echo ANALYZE_EPOCH.SH "Downloading UShER tree"
 # mkdir results/gisaid && cd results/gisaid
 usher_url=https://hgwdev.gi.ucsc.edu/~angie/caea02f/gisaidAndPublic.2022-09-14.masked.pb.gz
 usher_file=$(basename "$usher_url")
@@ -36,11 +36,11 @@ wget -O "results/gisaid/$usher_file" "$usher_url"
 # cd ../..
 
 # Process phylogenies
-echo "Processing phylogenies"
+echo ANALYZE_EPOCH.SH "Processing phylogenies"
 mfb="metadata_$d"
 python generate_epiToPublicAndDate.py --metadata-file-basename $mfb
 python scripts/preprocess_usher.py --tree-file-in results/gisaid/"$usher_file" --gisaid-metadata-file-in results/gisaid/"$mfb".tsv.gz
 
 # Fit model
-echo "Fitting model in background - progress in results/nohup.out"
+echo ANALYZE_EPOCH.SH "Fitting model in background - progress in results/nohup.out"
 nohup python scripts/mutrans.py > results/nohup.out 2>&1 &
