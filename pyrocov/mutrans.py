091d400bdf523631627972cb96aefed00d16fd10
bkotzen <bkotzen@mgh.harvard.edu>
2024-03-29 21:40:23 +0000

    update evro

--- pyrocov/mutrans.py
+++ pyrocov/mutrans.py
@@ -1,6 +1,7 @@
 # Copyright Contributors to the Pyro-Cov project.
 # SPDX-License-Identifier: Apache-2.0
 
+import pandas as pd
 import datetime
 import functools
 import logging
@@ -519,7 +520,7 @@ def model(dataset, model_type, *, forecast_steps=None):
         # First, read eve
         prior_fname = 'EVE/EVEPriors_Rescaled_2023-03-01.csv'
         prior = pd.read_csv(prior_fname)
-        prior['mutations'] = ['S:'+m for m in list(eve['mutations'])]
+        prior['mutations'] = ['S:'+m for m in list(prior['mutations'])]
         # Next, read features
         features_dict = torch.load('results/features.3000.1.pt')
         mutations = features_dict['aa_mutations']
@@ -531,16 +532,17 @@ def model(dataset, model_type, *, forecast_steps=None):
         center1 = [(c - np.mean(center1)) * s / np.std(center1) 
                    for c in center1]
 
+
         s = np.std(prior['evol_indices_boxcox_centered_rescaled'])
         center2 = [prior['evol_indices_boxcox_centered_rescaled'][i] 
-                   if m in list(eve['mutations']) else 0 
+                   if m in list(prior['mutations']) else 0 
                    for i,m in enumerate(mutations)]
         center2 = (center2 - np.mean(center2)) * s / np.std(center2)
         
         
         if "nofeatures" not in model_type:
             coef = pyro.sample(
-                "coef", dist.Laplace(torch.tensor(center1).float(), coef_scale).to_event(1)
+                "coef", dist.Laplace(torch.tensor(center1, device='cuda').float(), coef_scale).to_event(1)
             )  # [F]
         with clade_plate:
             if "localrate" in model_type:
